f69d16c5ddc7335e3e639a45fa1a62709e462374
Manolo <emmanuelsiame29@gmail.com>
2022-08-04 18:48:27 +0300

    added models

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Products(models.Model):
+    name = models.CharField(max_length=20, null=False, blank=False)
+    price = models.CharField(max_length=10, null=False, blank=False)
+    image = models.ImageField(null=False, blank=False)
